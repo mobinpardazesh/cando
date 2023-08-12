b9338e62304b6092f38f55d9be8590c2a3305909
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-12 23:51:32 +0330

    1402/5/21

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+# Create your  views here.
+def home(request):
+    # form = Customerform()
+    # if request.method == "POST":
+    #     form = Customerform(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #     # else:
+    #     #     form = Customerform
+    #     # context = {}
+    #     # context['form'] = Customerform()
+    #
+    # context = {'form': form}
+    return render(request, "home.html", {})
+
