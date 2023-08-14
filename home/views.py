bc614fc8fba1e241c96f9fc0af81ff0010d75105
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-14 18:13:25 +0330

    1402/5/23

--- home/views.py
+++ home/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView # Import TemplateView
 
+class Home(TemplateView):
+    template_name = "home.html"
 
 
 # Create your  views here.
-def home(request):
-    # form = Customerform()
-    # if request.method == "POST":
-    #     form = Customerform(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #     # else:
-    #     #     form = Customerform
-    #     # context = {}
-    #     # context['form'] = Customerform()
-    #
-    # context = {'form': form}
-    return render(request, "home.html", {})
+# def home(request):
+#     # form = Customerform()
+#     # if request.method == "POST":
+#     #     form = Customerform(request.POST)
+#     #     if form.is_valid():
+#     #         form.save()
+#     #     # else:
+#     #     #     form = Customerform
+#     #     # context = {}
+#     #     # context['form'] = Customerform()
+#     #
+#     # context = {'form': form}
+#     return render(request, "home.html", {})
 
