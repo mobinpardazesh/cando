937a6f74970f781cd5d91796edde1d945b82aee1
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 18:15:35 +0330

    1402/5/23

--- home/models.py
+++ home/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Student (models.Model):
+class Student (AbstractUser):
     student_name=models.CharField(max_length=100)
     student_familly=models.CharField(max_length=100)
     student_Age=models.IntegerField(default=0)
