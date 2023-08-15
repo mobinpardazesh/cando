cd7caa6b41f96ee35681362569f1072b8c480c35
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 10:04:50 +0330

    1402/5/23

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ app_name="cando"
 urlpatterns = [
     path('', views.Home.as_view(),name="home"),
     path("register", views.register, name="register"),
-    path("login", views.login(), name="login"),
+    path("login", views.login, name="login"),
 
 ]
 
