ef8ade2da5e516aa2fac67f16bcae2589eb0bcd6
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 11:51:19 +0330

    1402/5/23

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView # Import TemplateView
 
 
 from django.shortcuts import  render, redirect
-from .forms import NewUserForm
+from .forms import RegisterForm
 from django.contrib.auth import login
 from django.contrib import messages
 
@@ -11,14 +11,14 @@ class Home(TemplateView):
     template_name = "home.html"
 def register(request):
 	if request.method == "POST":
-		form = NewUserForm(request.POST)
+		form = RegisterForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
 			return redirect("main:homepage")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = NewUserForm()
+	form = RegisterForm()
 	return render (request=request, template_name="register.html", context={"register_form":form})
 def login (request):
 	return render(request,"login.html",{})
