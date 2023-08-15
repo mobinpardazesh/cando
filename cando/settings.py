7ee057becbd51105951f77ea98a74e6efd305d2a
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 14:02:31 +0330

    1402/5/23

--- cando/settings.py
+++ cando/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'crispy_forms',
 
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +125,5 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
