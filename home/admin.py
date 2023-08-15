da58d86e228276238057fa75fa34a2c01c4a01c4
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 17:57:05 +0330

    1402/5/23

--- home/admin.py
+++ home/admin.py
@@ -6,5 +6,5 @@ import home.models
 @admin.register(home.models.Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = (
-    "student_name", "student_familly", "student_Age", "student_fathername", "student_email","USERNAME_FIELD", "student_password",
+    "student_name", "student_familly", "student_Age", "student_fathername", "student_email","USERNAME_FIELD",
     "student_creatdate")
