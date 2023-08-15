50b8df499226e59c10f3867e33591eec9eb8ba79
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 10:11:15 +0330

    1402/5/23

--- home/forms.py
+++ home/forms.py
@@ -5,15 +5,14 @@ from django.contrib.auth.models import User
 
 # Create your forms here.
 
-class NewUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
 	email = forms.EmailField(required=True)
-
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
 
 	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
+		user = super(RegisterForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 		if commit:
 			user.save()
