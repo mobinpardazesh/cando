0c39bb08db6dedcc5a155aec3b65f12a261945a6
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-15 14:30:51 +0330

    1402/5/23

--- cando/settings.py
+++ cando/settings.py
@@ -125,5 +125,5 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CRISPY_TEMPLATE_PACK = 'uni_form'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
