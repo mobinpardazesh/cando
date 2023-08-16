ef052b6f974888fad99ab31e603eeae1f5ef5e02
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-16 18:01:24 +0330

    1402/5/23

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 import datetime
 from django import forms
+from django.forms import ModelForm
 from home.models import Student
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -7,18 +8,17 @@ from django.contrib.auth.models import User
 
 # Create your forms here.
 
-class RegisterForm(forms.Form):
-	student_name = forms.CharField(max_length=100)
-	student_familly = forms.CharField(max_length=100)
-	student_Age = forms.IntegerField(max_value=100)
-	student_fathername = forms.CharField(max_length=100)
-	student_username=forms.CharField(max_length=100)
-	student_email = forms.EmailField(required=True)
-	# student_birthdate = forms.DateTimeField()
+class RegisterForm(ModelForm):
+	# student_name = forms.CharField(max_length=100)
+	# student_familly = forms.CharField(max_length=100)
+	# student_Age = forms.IntegerField(max_value=100)
+	# student_fathername = forms.CharField(max_length=100)
+	# student_username=forms.CharField(max_length=100)
+	# student_email = forms.EmailField(required=True)
+	# # student_birthdate = forms.DateTimeField()
 	class Meta:
 		model = Student
 		fields = ("student_name", "student_familly", "student_Age", "student_fathername","student_username","student_email")
-
 	def save(self, commit=True):
 		user = super(RegisterForm, self).save(commit=False)
 		user.student_name = self.cleaned_data['student_name']
