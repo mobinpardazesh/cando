dd2e525c7b5c6bae5068cfc49b5772ae967a6224
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-17 22:07:45 +0330

    1402/5/21

--- home/views.py
+++ home/views.py
@@ -22,19 +22,20 @@ def register(request):
     return render(request=request, template_name="register.html", context={"register": Student_Register_Form})
 
 
-def login(request):
-    return render(request, "login.html", {})
-# Create your  views here.
-# def home(request):
-#     # form = Customerform()
-#     # if request.method == "POST":
-#     #     form = Customerform(request.POST)
-#     #     if form.is_valid():
-#     #         form.save()
-#     #     # else:
-#     #     #     form = Customerform
-#     #     # context = {}
-#     #     # context['form'] = Customerform()
-#     #
-#     # context = {'form': form}
-#     return render(request, "home.html", {})
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="main/login.html", context={"login_form":form})
