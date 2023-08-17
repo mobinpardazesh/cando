568ef1844f6ed5191389c2eecbb6ccabe5b8bd91
mobinpardazesh <mobinpardazesh@gmail.com>
2023-08-17 18:30:02 +0330

    1402/5/23

--- home/views.py
+++ home/views.py
@@ -1,24 +1,30 @@
-from django.views.generic import TemplateView # Import TemplateView
-from django.shortcuts import  render, redirect
-from home.forms import RegisterForm
-from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView  # Import TemplateView
+
+from home.forms import Student_Register_Form
+
 
 class Home(TemplateView):
     template_name = "home.html"
+
+
 def register(request):
-	if request.method == "POST":
-		form = RegisterForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("cando:home")
-		messages.error(request, "Unsuccessful registration. Invalid information.")
-	form = RegisterForm()
-	return render (request=request, template_name="register.html", context={"register":form})
-def login (request):
-	return render(request,"login.html",{})
+    if request.method == "POST":
+        form = Student_Register_Form(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("cando:home")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = Student_Register_Form()
+    return render(request=request, template_name="register.html", context={"register": form})
+
+
+def login(request):
+    return render(request, "login.html", {})
 # Create your  views here.
 # def home(request):
 #     # form = Customerform()
@@ -33,4 +39,3 @@ def login (request):
 #     #
 #     # context = {'form': form}
 #     return render(request, "home.html", {})
-
