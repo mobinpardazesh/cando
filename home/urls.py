0b5485d316c42b1e4073ce19bb93cd851105090c
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-08-18 18:46:23 +0330

    1402/5/21

--- home/urls.py
+++ home/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from . import views
 app_name="cando"
 urlpatterns = [
-    path('', views.Home.as_view(),name="home"),
+    # path('', views.Home.as_view(),name="home"),
+    path('', views.home,name="home"),
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('check-email/', views.check_email, name="check-email"),
-    path('forget-password/', views.forget_password, name="forget-password"),
-    path('reset-password/', views.reset_password, name="reset-password"),
+    path('forget-password/', views.forget_password, name="forget_password"),
+    path('reset-password/', views.reset_password, name="reset_password"),
 
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,51 +1,59 @@
-from django.contrib import messages
-from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView  # Import TemplateView
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
-from home.forms import Student_Register_Form ,Student_Login_Form
+from django.contrib.auth.forms import UserCreationForm
 
+# class Home(TemplateView):
+#     template_name = "home.html"
+def home (request):
+	return render(request,template_name="home.html",context={"home":home})
 
-class Home(TemplateView):
-    template_name = "home.html"
-
+# def signup(request):
+#     if request.method == "POST":
+#         student_Register_Form = Student_Register_Form(request.POST)
+#         if student_Register_Form.is_valid():
+#             new_student = student_Register_Form.save()
+#             # login(request, new_student)
+#         messages.error(request, "Unsuccessful registration. Invalid information.")
+#         return redirect("cando:register")
+#     messages.success(request, "Registration successful.")
+#     return render(request=request, template_name="signup.html", context={"register": Student_Register_Form})
 
 def signup(request):
-    if request.method == "POST":
-        student_Register_Form = Student_Register_Form(request.POST)
-        if student_Register_Form.is_valid():
-            new_student = student_Register_Form.save()
-            # login(request, new_student)
-        messages.error(request, "Unsuccessful registration. Invalid information.")
-        return redirect("cando:register")
-    messages.success(request, "Registration successful.")
-    return render(request=request, template_name="signup.html", context={"register": Student_Register_Form})
-
-
-def login(request):
-	if request.method == "POST":
-		student_login_Form = Student_Login_Form(request)
-		if student_login_Form.is_valid():
-			username = student_login_Form.cleaned_data.get('student_username')
-			password = student_login_Form.cleaned_data.get('student_password')
-			student = authenticate(username=username, password=password)
-			if student is not None:
-				login(request, student)
-				messages.info(request, f"You are now logged in as {username}.")
-				return redirect("homepage")
-			else:
-				messages.error(request,"Invalid username or password.")
-		else:
-			messages.error(request,"Invalid username or password.")
-	form = Student_Login_Form()
-	return render(request=request, template_name="login.html", context={"login":Student_Login_Form})
+    signup_form = UserCreationForm(request.POST)
+    if signup_form.is_valid():
+        signup_form.save()
+        username = signup_form.cleaned_data.get('username')
+        password = signup_form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    context = {
+        'signup_form': signup_form
+    }
+    return render(request, 'signup.html', context)
+# def login(request):
+# 	if request.method == "POST":
+# 		student_login_Form = Student_Login_Form(request)
+# 		if student_login_Form.is_valid():
+# 			username = student_login_Form.cleaned_data.get('student_username')
+# 			password = student_login_Form.cleaned_data.get('student_password')
+# 			student = authenticate(username=username, password=password)
+# 			if student is not None:
+# 				login(request, student)
+# 				messages.info(request, f"You are now logged in as {username}.")
+# 				return redirect("homepage")
+# 			else:
+# 				messages.error(request,"Invalid username or password.")
+# 		else:
+# 			messages.error(request,"Invalid username or password.")
+# 	form = Student_Login_Form()
+# 	return render(request=request, template_name="login.html", context={"login":Student_Login_Form})
 
 def  check_email(request):
 	return  render(request,template_name="check-email.html",context="check-email":Check_Email_Form)
 
 def  forget_password(request):
-	return  render(request,template_name="check-email.html",context="forget_password":Forget_Password_Form)
+	return  render(request,template_name="forget-password.html",context="forget_password":Forget_Password_Form)
 
 def  reset_password(request):
 	return  render(request,template_name="reset-password.html",context="reset_password":Reset_Password_Form)
